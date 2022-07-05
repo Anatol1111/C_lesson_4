1afea7e31d4818ac5f7196a63bd508c93d3c0735
Anatoliy Vlasenko <anatoliy.vlasko@mail.ru>
2022-07-05 22:07:22 +0600

    Homework_2 modiffied

---
+++ Lesson_4/Homework_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 2: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+
+int Num = int.Parse(Console.ReadLine());
+int sum = 0;
+while (Num > 0)
+{
+    sum = sum + Num % 10;
+    Num /= 10;
+}
+Console.WriteLine(sum);
