6c3a1e4a6b6a2318cb5729ed436f09b9d4077d28
Anatoliy Vlasenko <anatoliy.vlasko@mail.ru>
2022-07-05 14:31:03 +0600

    t_1-t_4 modiffied

---
+++ Lesson_4/t_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+int SumNum(int num)
+{
+    int all_Sum = 0;
+    for(int i = 1; i <= num; i++)
+    {
+        all_Sum += i;
+    }
+    return all_Sum;
+}
+
+Console.WriteLine(SumNum(7));
+Console.WriteLine(SumNum(4));
---
+++ Lesson_4/t_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+
+int PNum(int num)
+{
+    int all_Sum = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        all_Sum *= i;
+    }
+    return all_Sum;
+}
+
+Console.WriteLine(PNum(5));
+Console.WriteLine(PNum(4));
